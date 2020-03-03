022549a9b7a16b817361fce3f9c1b9007505c4af
systemt98 <61408022+systemt98@users.noreply.github.com>
2020-03-03 16:13:28 +0200

    deposit mail

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -32,7 +32,7 @@ use Illuminate\Http\Request;
 
 use App\Http\Traits\CPTrait;
 
-use App\Mail\NewNotification;
+// use App\Mail\NewNotification;
 use App\Mail\htmlNotification;
 use App\Mail\newroi;
 use App\Mail\endplan;
@@ -559,11 +559,20 @@ class Controller extends BaseController
          //send email notification
         $objDemo = new \stdClass();
         $objDemo->message = "$user->name, This is to inform you that your deposit of $settings->currency $deposit->amount has been received and confirmed.";
-        $objDemo->sender = "$settings->site_name";
+        $objDemo->receiver_name = "$user->name";
+        $objDemo->url = "https://privilege-coin.com/";
+        $objDemo->sender = $settings->site_name;
+        $objDemo->subject ="Successful withdrawal";
         $objDemo->date = \Carbon\Carbon::Now();
-        $objDemo->subject = "Deposit processed!";
+      
+        // Mail::bcc($user->email)->send(new NewNotification($objDemo));
+        Mail::to($data['email'])->send(new htmlNotification($objDemo));
+
+        // $objDemo->sender = "$settings->site_name";
+        // $objDemo->date = \Carbon\Carbon::Now();
+        // $objDemo->subject = "Deposit processed!";
             
-        Mail::bcc($user->email)->send(new NewNotification($objDemo));
+        // Mail::bcc($user->email)->send(new NewNotification($objDemo));
     
       }
 
