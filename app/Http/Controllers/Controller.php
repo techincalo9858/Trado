a3a3c3ab18ec8f39de2ac94ee192c8e9191c01b4
mreouven-study <37777368+mreouven-study@users.noreply.github.com>
2020-03-06 08:52:18 +0200

    data

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -362,7 +362,7 @@ class Controller extends BaseController
           'sellers' => users::where('type', '=', '2')->paginate(10),
           'settings' => settings::where('id', '=', '1')->first(),
         );
-        // dd($data);
+        dd($data->balance->bal);
         return view('users')
           ->with($data);
         
