82b8d88a8707950e87a4c446d37d2506f4bd69bf
mreouven-study <37777368+mreouven-study@users.noreply.github.com>
2020-03-12 13:42:36 +0200

    Update index.blade.php

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -24,6 +24,18 @@
   <link rel="stylesheet" href="{{asset('ot_temp1/css/demo.css')}} ">
   <link rel="stylesheet" href="{{asset('ot_temp1/css/animate.css')}}">
   <script src="{{asset('js/modernizr.js')}}"></script> <!-- Modernizr -->
+  <style>
+.masthead {
+    margin-top: -46px;
+    padding-bottom: 57rem;
+
+    height: 100vh;
+    min-height: 504px;
+    background-image: url(https://privilege-coin.com/ot_temp1/img/acceuil.png);
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+}</style>
 
 </head>
 
@@ -102,31 +114,23 @@
 
           </div>
         </nav>
-<style>
-.masthead {
-  height: 100vh;
-  min-height: 500px;
-  background-image: url('https://source.unsplash.com/BtbjCFUvBXs/1920x1080');
-  background-size: cover;
-  background-position: center;
-  background-repeat: no-repeat;
-}</style>
+
 
         <!-- .........................................................................This section is for Sliding Text in the begining of the Page -->
-        <header class="masthead">
-          <div class="container h-100">
-            <div class="row h-100 align-items-center">
-              <div class="col-12 text-center">
-                <h1 class="font-weight-light">Vertically Centered Masthead Content</h1>
-                <p class="lead">A great starter layout for a landing page</p>
-              </div>
-            </div>
-          </div>
-        </header>
+        
 
       </div>
     </div>
   
+  </header>
+  <header class="masthead">
+          <div class="container h-100">
+            <div class="row h-100 align-items-center">
+              <div class="col-12 text-center">
+
+              </div>
+            </div>
+          </div>
   </header>
 
 
